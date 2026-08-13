ba3fa282fc79d874ec793e9aedf9870406b46e54
praveen520310 <144161277+praveen520310@users.noreply.github.com>
2026-08-13 17:01:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, KrishnaVcube1234567");
      System.out.println("Hello, devops");
-     System.out.println("Hello, devopsbykrishnaareddysir"); 
+     System.out.println("Hello, devopsbykrishnaareddysirr"); 
 
   }
 }
